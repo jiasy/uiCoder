1ee168fc6098aab87bbd288fc8c7d27a33537251
jiasy <jiasy@xiexiaoxiao.local>
2018-03-08 23:30:24 +0800

    文件夹结构变更

--- codeFrame/lua/threeGame/MainScene.lua
+++
@@ -1,9 +0,0 @@
-[<File_doc|ui|/src/app/views/MainScene.lua|replace>]
-local MainScene = class("MainScene", cc.load("mvc").ViewBase)
-
-function MainScene:onCreate()
-	self:addChild(uiControl:getInstance().stageContainer)
-	uiControl:getInstance():jumpTo("develop","main"):init({})
-end
-
-return MainScene
